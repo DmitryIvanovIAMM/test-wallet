fca2d05419f29b323e3b85e3996cb41bfcd36a4f
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-23 06:28:22 +0200

    TW-4 Add tests for getDateString() helper

--- src/types/dates.spec.ts
+++ src/types/dates.spec.ts
@@ -1,12 +1,25 @@
-import { getDateString } from '@/types/dates';
+import { daysOfWeek, getDateString } from '@/types/dates';
 
 describe('getDateString() helper should', () => {
+  it('return date ISO string for the date not within the last week', () => {
+    for (let i = 0; i < 7; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() - i);
+      const dateString = getDateString(date);
+      expect(daysOfWeek.includes(dateString)).toBe(true);
+    }
+  });
+
   it('return day of the week for the date within the last week', () => {
-    // Arrange
     const date = new Date('2022-01-01');
-    // Act
-    const result = getDateString(date);
-    // Assert
-    expect(result).toBe('1/1/2022');
+    const dateString = getDateString(date);
+    expect(dateString).toBe('1/1/2022');
+  });
+
+  // this not specified in test task
+  it('return date ISO string for the date in future', () => {
+    const dateFuture = new Date('2037-01-01');
+    const resultDateFuture = getDateString(dateFuture);
+    expect(resultDateFuture).toBe('Thursday');
   });
 });
--- src/types/dates.ts
+++ src/types/dates.ts
@@ -1,4 +1,12 @@
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
 export const getDateString = (date: Date | string) => {
   const incomingDate = new Date(date);

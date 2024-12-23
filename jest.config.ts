8dd4dc22a1b631a222c5734e4133d26ad7637566
Zayats <dmitry.ivanov.iamm@gmail.com>
2024-12-23 17:26:39 +0200

    TW-7 Set UTC timezone for jest

--- jest.config.ts
+++ jest.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  // globalSetup: undefined,
+  globalSetup: './global-setup.js',
 
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: undefined,
